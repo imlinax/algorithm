7908d403101a1f96455a3135ca7aa7583b231008
somebody <somebody@somebody-VirtualBox.(none)>
2016-06-19 11:05:31 +0800

    sort array with unique elements

--- find/find.c
+++ find/find.c
@@ -4,6 +4,23 @@
 
 #include "find.h"
 #include "sort.h"
+void swap(int *pa,int *pb)
+{
+    int temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+}
+void gen_unirandom_array(int *parray,int size)
+{
+    for(int i = 0; i < size;++i)
+    {
+	parray[i] = i;
+    }
+    for(int i = 0; i <size; ++i)
+    {
+	swap(&parray[i],&parray[rand()%size]);
+    }
+}
 void gen_randomarray(int *parray,int size)
 {
     srand(time(0));
@@ -22,22 +39,25 @@ void print_array(int *parray,int size)
 }
 int main()
 {
-    int arraysize = 10;
+    int arraysize = 10 * 1000;
     int *parray = (int*)malloc(sizeof(int) * arraysize);
     // 生成随机数组
-    gen_randomarray(parray,arraysize);
+    //gen_randomarray(parray,arraysize);
+    gen_unirandom_array(parray,arraysize);
     // 打印数组  
     //print_array(parray,arraysize);
+
+    // sort array
+    sort_shell(parray,arraysize);
     //保存需要查找的数据
+    srand(time(0));
     int findindex = rand() % arraysize;
     int findval = parray[findindex];
 	
     int findedindex = -1;
     findedindex = find_seq(parray,arraysize,findval);
     printf("find_seq    :finedindex:%d,realindex:%d\n",findedindex,findindex);
-    // sort array
-    sort_shell(parray,arraysize);
-    print_array(parray,arraysize);
+    //print_array(parray,arraysize);
     findedindex = find_binary(parray,arraysize,findval);
     printf("find_binary :finedindex:%d,realindex:%d\n",findedindex,findindex);
 
